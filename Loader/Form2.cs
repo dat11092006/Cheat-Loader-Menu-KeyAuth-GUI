c8126ee4c57510ef5efcecbf6d25eb5ac9c34b7c
ersfgfcvb <whrkjdfywer8@outlook.com>
2025-01-14 07:31:30 -0800

    Form2.cs

---
+++ Loader/Form2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace loader
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            
+            InitializeComponent();
+            TextBox.Enabled = false; // true
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2ControlBox1_Click(object sender, EventArgs e)
+        {
+            int pid = Process.GetCurrentProcess().Id;
+            Process process = Process.GetProcessById(pid);
+            process.Kill();
+        }
+    }
+}
